46b4615ae8a687c4ee1637a9a768b83be845e494
howard <howard.engelhart@outlook.com>
2015-06-17 11:09:32 -0400

    removed old sql files

--- sql/mr_missing_experiences.sql
+++
@@ -1,7 +0,0 @@
-﻿select hostname,experience_id 
-from fct.mr_embeds_by_device
-where experience_id not in (
-	select experience_id from dim.minireel group by experience_id order by experience_id
-) 
-group by hostname,experience_id
-order by hostname,experience_id
